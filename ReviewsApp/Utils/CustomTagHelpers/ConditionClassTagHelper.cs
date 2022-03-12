787de433a952d56a6aeca124777e834536a22f8b
Pavel Morozov <veaberji@gmail.com>
2022-03-12 14:33:18 +0300

    Move StarRating/Like logic to TagHelpers

---
+++ ReviewsApp/Utils/CustomTagHelpers/ConditionClassTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReviewsApp.Utils.CustomTagHelpers
+{
+    [HtmlTargetElement(Attributes = TagHelperPrefix + "*")]
+    public class ConditionClassTagHelper : TagHelper
+    {
+        private const string TagHelperPrefix = "condition-class-";
+
+        [HtmlAttributeName("class")]
+        public string CssClass { get; set; }
+
+        private IDictionary<string, bool> _classValues;
+
+        [HtmlAttributeName("", DictionaryAttributePrefix = TagHelperPrefix)]
+        public IDictionary<string, bool> ClassValues => _classValues ??=
+                    new Dictionary<string, bool>(StringComparer.OrdinalIgnoreCase);
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var items = ClassValues.Where(e => e.Value)
+                .Select(e => e.Key).ToList();
+
+            if (!string.IsNullOrEmpty(CssClass))
+            {
+                items.Insert(0, CssClass);
+            }
+
+            if (items.Any())
+            {
+                var classes = string.Join(" ", items);
+                output.Attributes.Add("class", classes);
+            }
+        }
+    }
+}
---
+++ ReviewsApp/Utils/CustomTagHelpers/ConditionHiddenTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ReviewsApp.Utils.CustomTagHelpers
+{
+    [HtmlTargetElement(Attributes = TagHelperName)]
+    public class ConditionHiddenTagHelper : TagHelper
+    {
+        private const string TagHelperName = "condition-hidden";
+
+        [HtmlAttributeName(TagHelperName)]
+        public bool IsFulfilled { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (IsFulfilled)
+            {
+                output.Attributes.Add("hidden", true);
+            }
+        }
+    }
+}
