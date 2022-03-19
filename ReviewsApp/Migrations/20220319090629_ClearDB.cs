e1a06f7742a1ae9a4289508305012766b141ca8b
Pavel Morozov <veaberji@gmail.com>
2022-03-19 12:23:09 +0300

    Clear DB

---
+++ ReviewsApp/Migrations/20220319090629_ClearDB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReviewsApp.Migrations
+{
+    public partial class ClearDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM [dbo].[Tags]
+                DELETE FROM [dbo].[Images]
+                DELETE FROM [dbo].[Comments]
+                DELETE FROM [dbo].[Likes]
+                DELETE FROM [dbo].[Reviews]
+                DELETE FROM [dbo].[UserGrades]
+                DELETE FROM [dbo].[Products]
+
+                DELETE FROM [dbo].[AspNetRoles]
+                DELETE FROM [dbo].[AspNetUserLogins]
+                DELETE FROM [dbo].[AspNetUsers] 
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
